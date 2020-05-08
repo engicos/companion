c71b6f8515e97b61dd2089306e71566bf5a7363b
Julius Alphonso <juliusalphonso.09@gmail.com>
2020-05-08 20:28:44 +0530

    Use `style jsx` to style EventCards

--- components/eventCard.js
+++ components/eventCard.js
@@ -1,14 +1,49 @@
-const EventCard = (props) => {
-  var startTime =
-    props.startTime < 13 ? props.startTime + "am" : props.startTime - 12 + "pm";
-  var endTime =
-    props.endTime < 13 ? props.endTime + "am" : props.endTime - 12 + "pm";
+const EventCard = ({event}) => {
+  event.Start = event.Start.slice(0,2);
+  event.End = event.End.slice(0,2);
+  var Start =
+    event.Start < 13 ? event.Start + "am" : event.Start - 12 + "pm";
+  var End =
+    event.End < 13 ? event.End + "am" : event.End - 12 + "pm";
   return (
     <div className="eventcard">
       <div></div>
-      <div className="duration">{`${startTime} - ${endTime}`}</div>
-      <div className="time">{startTime}</div>
-      <div className="topic">{props.eventName}</div>
+      <div className="duration">{`${Start} - ${End}`}</div>
+      <div className="time">{event.Start}</div>
+      <div className="topic">{event.subject.Title}</div>
+      <style jsx>{`
+        .eventcard {
+            display: grid;
+            grid-template:  [null] "time duration" 1em [duration]
+                            [time] "time topic"    3em [event title]
+                                /   3em  auto;
+            border: 1px solid;
+            border-radius: 10px;
+            margin: 0.5rem;
+        }
+
+        .eventcard > .duration {
+            grid-area: duration;
+            font-style: italic;
+            text-align: left;
+            padding: .5em 0em 0.5em 1em;
+        }
+
+        .eventcard > .time {
+            grid-area: time;
+            text-align: right;
+            padding: 20%;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 2em;
+        }
+
+        .eventcard > .topic {
+            grid-area: topic;
+            text-align: left;
+            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            padding: 1em;
+        }
+      `}</style>
     </div>
   );
 };
