b12622a6327c9d8e3863294dd7e93037bfe0de4e
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2020-07-26 13:24:04 +0530

    Retrieve the correct jwt cookie

--- utils/apollo.js
+++ utils/apollo.js
@@ -18,7 +18,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, {headers}) => {
   // get the authentication token from local storage if it exists
-  const token = cookies.jwt
+  const token = cookies.get("jwt")
 
   // return the headers to the context so httpLink can read them
   return {
